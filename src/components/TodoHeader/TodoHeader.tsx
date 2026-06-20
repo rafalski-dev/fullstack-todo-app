0cb41a85e2ed6a44a03e63e97f8bb5f61b21a8b8
Jakub <jakubrafalski96@gmail.com>
2026-06-20 12:31:46 +0100

    Todo App has been finished.

--- src/components/TodoHeader/TodoHeader.tsx
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -4,8 +4,15 @@ import { Wrapper } from '../Wrapper/Wrapper';
 import { supabase } from '../../lib/supabase';
 import { useEffect, useRef, useState } from 'react';
 import { type Session } from '@supabase/supabase-js';
+import { Logo } from '../Logo/Logo';
+import { Link } from 'react-router-dom';
+import { SecondaryCTA } from '../SecondaryCTA/SecondaryCTA';
 
-export function TodoHeader({ session }: { session: Session }) {
+type SessionProps = {
+	session: Session | null;
+};
+
+export function TodoHeader({ session }: SessionProps) {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const menuRef = useRef<HTMLDivElement>(null);
 
@@ -25,20 +32,36 @@ export function TodoHeader({ session }: { session: Session }) {
 		<header className={styles.header}>
 			<Wrapper>
 				<div className={styles.headerContent}>
+					{session ? (
+						<Logo />
+					) : (
+						<Link to='/'>
+							<Logo />
+						</Link>
+					)}
 					<div className={styles.box} ref={menuRef}>
-						<button className={styles.avatar} onClick={() => setIsMenuOpen(prevMenu => !prevMenu)}>
-							{`${session.user.user_metadata.name.at(0)}${session.user.user_metadata.surname.at(0)}`}
-						</button>
+						{session ? (
+							<button className={styles.avatar} onClick={() => setIsMenuOpen(prevMenu => !prevMenu)}>
+								{`${session?.user.user_metadata.name.at(0)}${session?.user.user_metadata.surname.at(0)}`}
+							</button>
+						) : (
+							<SecondaryCTA path='/'>Home</SecondaryCTA>
+						)}
 						{isMenuOpen && (
 							<div className={styles.userMenu}>
 								<div className={styles.userData}>
 									<span
 										className={
 											styles.fullname
-										}>{`${session.user.user_metadata.name} ${session.user.user_metadata.surname}`}</span>
-									<span className={styles.email}>{session.user.user_metadata.email}</span>
+										}>{`${session?.user.user_metadata.name} ${session?.user.user_metadata.surname}`}</span>
+									<span className={styles.email}>{session?.user.user_metadata.email}</span>
 								</div>
-								<button className={styles.btn} onClick={() => supabase.auth.signOut()}>
+								<button
+									className={styles.btn}
+									onClick={() => {
+										supabase.auth.signOut();
+										setIsMenuOpen(false);
+									}}>
 									<IconLogout size={20} strokeWidth={1.8} />
 									Sign out
 								</button>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { createRoot } from 'react-dom/client';
 import './globals.css';
 import App from './App.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { AuthPanel } from './pages/Auth/AuthPanel.tsx';
+import { TodoApp } from './pages/TodoApp/TodoApp.tsx';
 import { Home } from './pages/home/Home.tsx';
 
 const router = createBrowserRouter([
-	{ path: '/', element: <App />, children: [{ index: true, element: <Home /> }] },
-	{ path: '/auth', element: <AuthPanel /> }
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: '/offer', element: <p>Oferta</p> }
+		]
+	},
+	{ path: '/auth', element: <TodoApp /> }
 ]);
 
 createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
